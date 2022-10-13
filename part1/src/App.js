63d64cccdbcce90a1fc310ba40a17a3f96573f6c
MauricioM3jia <30279046+MauricioM3jia@users.noreply.github.com>
2022-10-13 14:25:29 -0600

    we created the basics components and did some examples of how react works

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,8 +1,38 @@
+const Hello = (props)=>{
+    return (
+      <div>
+          <p>
+              Hello {props.name},you are {props.age} years old
+          </p>
+      </div>
+    );
+}
+
+const Footer =()=>{
+   
+  return( <div>
+
+    <p>greeting app created by <a href='https://github.com/mluukkai'>mluukkai</a></p> 
+ </div>);
+ 
+}
 
 function App() {
+
+  console.log('hello components')
+  const now = new Date()
+  const a = 10
+  const b = 20
+  const name = 'Peter'
+  const age=10;
   return (
    <div>
-      <p>Hello world</p>
+      <h1>Hello Greetings</h1>
+      <p>hello world, it is {now.toString()}</p>
+      <p>{a} plus {b} is {a+b}</p>
+      <Hello name='Roger' age={26+10}/>
+      <Hello name={name} age={age} />
+      <Footer />
    </div>
   );
 }
