926afb370c9304eafe1cbff499c40e2a2f755805
MauricioM3jia <30279046+MauricioM3jia@users.noreply.github.com>
2022-10-14 15:40:39 -0600

    did some examples of how to render in a basic way without useState

--- part1/part1c/src/App.js
+++ part1/part1c/src/App.js
@@ -1,7 +1,33 @@
 
+const Hello = (props) => {
+  console.log(props);
+
+const {name,age} = props;
+    const bornYear= ()=> new Date().getFullYear()-age;
+  return (  
+    <div>
+        <p> {name}, you are {age} years old</p>
+
+        <p>So you were probably born in {bornYear()}</p>
+    </div>
+  );
+}
+ 
+
+
 const App = () => {
+ 
+  const name = 'Peter'
+  const age = 10
+  
   return ( 
     <>
+    <div>
+      <h1> Greetings</h1>
+      <Hello name='Henry' age={24}/>
+      <Hello name={name} age={age}/>
+      
+    </div>
     </>
    );
 }
