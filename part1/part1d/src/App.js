e02390ff1b6d23a33b3f9b8afbd3ebbf797a24a7
MauricioM3jia <30279046+MauricioM3jia@users.noreply.github.com>
2022-10-17 16:28:16 -0600

    part 1b has been finnished,we learned morea bout props concepts, now we will continue with the exercises.

---
+++ part1/part1d/src/App.js
@@ -0,0 +1,81 @@
+import { useState } from "react";
+import Button2 from './components/Button';
+
+const History = (props) => {
+  if(props.allClicks.length === 0){
+      return (
+          <div>
+              the app is used by pressing the buttons
+          </div>
+      )
+  }
+  return (  
+  <div>
+      button press history: {props.allClicks.join('')}
+  </div>
+  );
+}
+  const Button = (props)=>{
+    console.log(props);
+    const {onClick,text}= props;
+      return (<button onClick={onClick}>{text}</button>)    
+  }
+
+const App = () => {
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
+  const [value,setValue]= useState(10)
+  
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat("L"));
+    setLeft(left + 1);
+  };
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat("R"));
+    setRight(right + 1);
+  };
+
+  const hello = (who)=>{
+    
+    return ()=>{
+        console.log(`Hello`,who);
+    }
+  }
+
+  const setToValue = (newValue)=>{
+    console.log('value now',newValue);
+    setValue(newValue)
+  }
+
+  return (
+    <>
+      <div>
+        {left}
+        <Button onClick={handleLeftClick} text='left'/>
+        <Button onClick={handleRightClick} text='right'/> 
+        {right}
+        
+        <History allClicks={allClicks}/>
+
+        {value}
+        <button onClick={hello('world')}>button</button>
+        <button onClick={hello('react')}>button</button>
+        <button onClick={hello('function')}>button</button>
+        
+        <div>
+            {value}
+
+            <Button2 handleClick={()=>setToValue(1000) } text ="thousand"/>
+            <Button2 handleClick={()=>setToValue(0)} text ="reset"/>
+            <Button2 handleClick={()=>setToValue(value + 1)} text ="increment"/>
+            
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
