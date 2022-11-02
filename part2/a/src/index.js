06df392e21508b13d3614f3ff31f0ed474881883
MauricioM3jia <30279046+MauricioM3jia@users.noreply.github.com>
2022-11-02 10:17:37 -0600

    part 2 file a has been created and completed

---
+++ part2/a/src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './App';
+const notes= [
+  {
+    id:1,
+    content:'HTML is easy',
+    date:'2022-11-02T30:31.098Z'
+  },
+  {
+    id:2,
+    content:'Browser can execute only Javascrypt',
+    date:'2022-11-02T30:31.098Z'
+  },
+  {
+    id:3,
+    content:'Get and POST are the most important methods of HTTP protocol',
+    date:'2022-11-02T30:31.098Z'
+  }
+];
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App notes={notes}/>
+  </React.StrictMode>
+);
+
+
