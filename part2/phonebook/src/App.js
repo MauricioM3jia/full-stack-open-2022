9f3a2c34c1935759542dd6e8d631ed92158c0d27
MauricioM3jia <30279046+MauricioM3jia@users.noreply.github.com>
2022-11-24 10:32:34 -0600

    exercise 2.11 completed

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,20 +1,24 @@
-import { useState } from 'react'
+import { useState ,useEffect} from 'react';
+import axios from 'axios'
 import Search from './components/Search.jsx'
 import PersonForm from './components/PersonForm.jsx'
 import Person from './components/Person.jsx'
 const App = () => {
 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+
+  const [persons,setPersons]=useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber,setNewNumber]= useState('');
   const [newSearch,setNewSearch]=useState('');
-
-
+  const apiUrl= "http://localhost:3001/persons";
+ 
+  useEffect(()=>{
+      async function fetchDataApi(){
+        const response = await axios.get(apiUrl);
+        console.log(response);
+        setPersons(response.data);
+      }fetchDataApi();
+  },[])
   const handleChange = setValue => e =>setValue(e.target.value)
 
   const addPersonInfo = (event)=>{
